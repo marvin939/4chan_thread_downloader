fba99c481b4c0e46ea5aaef52ff3b7852a6b7943
marvin939 <marvinCEF@gmail.com>
2017-11-19 20:16:32 +1300

    Changed project to thread_files

---
+++ thread_files/4tdl.py
@@ -0,0 +1,10 @@
+import sys
+
+from thread_files import climan
+
+if __name__ == '__main__':
+    args = climan.CLIMan().parser.parse_args(sys.argv[1:])
+    if hasattr(args, 'func'):
+        res = args.func(args)
+        if res is not None:
+            print(res)
