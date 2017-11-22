ea264e8e3ccf47de53d6e785ff6e2f253ca19053
marvin939 <marvinCEF@gmail.com>
2017-11-22 19:53:21 +1300

    Release 0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='thread_files',
     packages=['thread_files'],
-    version='0.1.1',
+    version='0.1.2',
     description='A 4chan thread downloader',
     author='Marvin Muyargas',
     author_email='',
